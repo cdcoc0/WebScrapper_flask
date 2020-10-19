296935db4c5fa810c0e70857bbb8e6940fbac26a
Jiyeon Choi <jiyunee13@gmail.com>
2020-10-19 03:40:02 +0000

    import render_template apply a html file try dynamic url

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask("Flask_Scrapper")
 
+
 @app.route("/")
 def home():
-  return "Welcome to Podo home!"
+  return render_template("index.html")
+
+#직접 html코드 사용 가능
+# def home():
+#   return "<h1>Job Search</h1>\
+#   <form><input placeholder='find the jobs' required /><button>Search</button>"
+
+#dynamic url
+# @app.route("/<username>")
+# def show_name(username):
+#   return f"Hello {username}"
 
 app.run(host="0.0.0.0")
